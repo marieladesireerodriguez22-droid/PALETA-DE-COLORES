bf8a5bb5ebba564236c75566b66e41cf13d1ac2b
marieladesireerodriguez22-droid <mariela.desiree.rodriguez22@gmail.com>
2026-05-19 19:40:48 -0300

    funcion de btn

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('generateBtn').addEventListener('click', function() {
+    const size = parseInt(document.getElementById('paletteSize').value);
+    const format = document.getElementById('colorFormat').value;
+    const container = document.getElementById('paletteContainer');
+
+    //limpair el contenedor
+    container.innerHTML = '';
+
+    
+}
