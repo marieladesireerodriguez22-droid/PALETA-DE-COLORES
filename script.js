ef0c2cc866b652f1c8af8b74f4e4323e95057f91
marieladesireerodriguez22-droid <mariela.desiree.rodriguez22@gmail.com>
2026-05-19 19:47:43 -0300

    se generan colores aleatorios en la pantalla

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ document.getElementById('generateBtn').addEventListener('click', function() {
     //limpair el contenedor
     container.innerHTML = '';
 
+    for (let i =0; i < size; i++) {
+        //GENERAR COLORES ALEATORIOS
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+
+        let colorString = '';
+        
+    }
+
     
 }
